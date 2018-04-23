11b753413f62e0d47cfd9a57108cc257b892972d
Julien Pons <j.pons.lyon@gmail.com>
2018-04-23 17:53:05 +0200

    foreign key

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,12 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    
+    /**
+     * @Route("/test/{name}", name="test")
+     */
+    public function testAction(Request $request, $name)
+    {
+        return $this->render('default/test.html.twig', ['name' => $name]);
+    }
 }
